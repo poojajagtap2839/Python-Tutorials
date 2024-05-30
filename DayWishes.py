31b802f7bc8f4055c56ec81ccf4e575fbc33b7b8
Pooja Jagtap <poojajagtap283@gmail.com>
2024-05-30 22:21:27 +0530

    updated variable in DayWishes

--- DayWishes.py
+++ DayWishes.py
@@ -4,12 +4,12 @@ import time
 fullTime=time.strftime('%H:%M:%S')
 hour=int(time.strftime('%H'))
 print(hour)
-Myname=input("Enter your good name: ").title()
+name=input("Enter your good name: ").title()
 
 if(hour>5 and hour<12):
-    print("Hello, Good Morning ",Myname)
+    print("Hello, Good Morning ",name)
 elif(hour>12 and hour<17):
-    print("Hello, Good Afternoon ", ourname)
+    print("Hello, Good Afternoon ", name)
 else:
-    print("Hello, Good Evening ", ourname)
+    print("Hello, Good Evening ", name)
 
