111e3450d86d67352ce2614c3f739232c079fbae
Pooja Jagtap <poojajagtap283@gmail.com>
2024-06-02 19:10:51 +0530

    EvenOdd Program

---
+++ EvenOdd.py
@@ -0,0 +1,12 @@
+def Check_Even_odd(n):
+    if (n%2==0):
+        print(n ,"is Even Number")
+    else:
+        print(n, "is Odd Number")
+
+
+
+
+number=int(input("Enter any number: "))
+Check_Even_odd(number)
+
